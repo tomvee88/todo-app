f732e64ab9e90558e000dda655859cfd24880778
tomissmee <tommyvong88@gmail.com>
2018-03-19 14:42:18 -0700

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,28 +1,12 @@
 const express = require('express');
 const hbs = require('hbs');
-const app = express();
 const fs = require('fs');
+
+const port = process.env.PORT || 3000; 
+const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-
-app.use((req, res, next) => {
-    let now = new Date().toString();
-    let log = `${now}: ${req.method} ${req.url}`;
-
-    console.log(log);
-    fs.appendFile('server.log', log + '\n', (err) => {
-        if(err) {
-            console.log('Unable to append file');
-        }
-    });
-
-    next();
-});
-
-// app.use((req, res, next) => {
-//     res.render('maintenance');
-// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -42,8 +26,6 @@ app.get('/about', (req, res) => {
         pageTitle: 'About Page'
     });
 });
-
-const port = 3000;
 
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
